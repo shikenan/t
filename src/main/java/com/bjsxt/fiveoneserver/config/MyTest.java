2ff741e20b6975b3a555b1c6b4c7f2a70b3c865f
shikenan <57400605+shikenan@users.noreply.github.com>
2020-07-09 16:10:30 +0800

    Update MyTest.java

--- src/main/java/com/bjsxt/fiveoneserver/config/MyTest.java
+++ src/main/java/com/bjsxt/fiveoneserver/config/MyTest.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 //开发更改
+//整理更改
 public class MyTest {
     public static void main(String[] args) throws InterruptedException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
 
